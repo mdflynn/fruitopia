67b68db651c298a0f2ebf04921cf6ae90ded8390
Mike Flynn <mdflynn34@outlook.com>
2021-09-04 07:38:31 -0600

    Add boilerplate and import to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.scss";
 
-import { Route, Switch } from "react-router-dom";
 import Nav from "../Nav/Nav";
+import HomePage from "../HomePage/HomePage";
+import FruitPage from "../FruitPage/FruitPage";
+
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <Nav />
+  return (
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
