533786a0f17997abd71fc15079f8ba418a7e4505
Mike Flynn <mdflynn34@outlook.com>
2021-09-04 12:19:10 -0600

    remove index file

--- src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import grape from "../assets/grape.jpg";
-import lime from "../assets/lime.jpg";
-import melon from "../assets/melon.jpg";
-import papaya from "../assets/papaya.jpg";
-import persimmon from "../assets/persimmon.jpg";
-
-const fruitImages = [grape, lime, melon, papaya, persimmon];
-
-export default fruitImages;
