a6628d386acf667fe9a2b7a685bcf65c683a5ee6
Mike Flynn <mdflynn34@outlook.com>
2021-09-04 11:59:05 -0600

    import/export images

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import grape from "../assets/grape.jpg";
+import lime from "../assets/lime.jpg";
+import melon from "../assets/melon.jpg";
+import papaya from "../assets/papaya.jpg";
+import persimmon from "../assets/persimmon.jpg";
+
+const fruitImages = [grape, lime, melon, papaya, persimmon];
+
+export default fruitImages;
