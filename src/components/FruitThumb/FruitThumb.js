405404edc78f9f9deaaf17d93bfe40b891334b4b
Mike Flynn <mdflynn34@outlook.com>
2021-09-04 11:48:46 -0600

    add in thumb data to display

--- src/components/FruitThumb/FruitThumb.js
+++ src/components/FruitThumb/FruitThumb.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./FruitThumb.scss";
 
-const FruitThumb = () => {
- return <h1>FruitThumb</h1>
+const FruitThumb = ({ details }) => {
+  const formatUrl = `${details.name.toLowerCase()}`;
+  const thumbImg = `https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/${formatUrl}.png`;
+
+  return (
+    <article>
+      <img src={thumbImg} />
+      <p>{details.name}</p>
+    </article>
+  );
 };
 
 export default FruitThumb;
+// image issues for:
+// grape, lime, persimmon, papaya, melon
