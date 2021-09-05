09a1092c122d8a0d285a9933666c275e2cbb546c
Mike Flynn <mdflynn34@outlook.com>
2021-09-04 19:13:20 -0600

    add fruit deatils

--- src/components/FruitPage/FruitPage.js
+++ src/components/FruitPage/FruitPage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./FruitPage.scss";
 
-const FruitPage = () => {
-    return <h1>FruitPage</h1>
-}
+const FruitPage = ({ fruitDetails }) => {
+  return (
+    <section className="fruit-section">
+      <img src={fruitDetails.image} />
+      <div className="fruit-details">
+        <div>
+          <p>{fruitDetails.name}</p>
+          <p>{fruitDetails.genus}</p>
+          <p>{fruitDetails.family}</p>
+          <p>{fruitDetails.order}</p>
+          <p>{fruitDetails.family}</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default FruitPage
+export default FruitPage;
