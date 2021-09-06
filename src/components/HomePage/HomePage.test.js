504f79d3332af778bee5011f591aa8999e620f7f
Mike Flynn <mdflynn34@outlook.com>
2021-09-05 20:31:39 -0600

    add test for fruit names

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router";
 
@@ -9,42 +9,51 @@ import fetchFruitInformation from "../../apiCalls";
 jest.mock("../../apiCalls.js");
 
 const sampleFruits = [
-    {
-        genus: "Citrus",
-        name: "Lemon",
-        id: 26,
-        family: "Rutaceae",
-        order: "Sapindales",
-        nutritions: {
-          carbohydrates: 9,
-          protein: 1.1,
-          fat: 0.3,
-          calories: 29,
-          sugar: 2.5,
-        },
-      },{
-        genus: "Malus",
-        name: "Apple",
-        id: 26,
-        family: "Rosaceae",
-        order: "Rosales",
-        nutritions: {
-          carbohydrates: 11.4,
-          protein: 0.3,
-          fat: 0.4,
-          calories: 52,
-          sugar: 10.3,
-        },
-      };
-]
+  {
+    genus: "Citrus",
+    name: "Lemon",
+    id: 26,
+    family: "Rutaceae",
+    order: "Sapindales",
+    nutritions: {
+      carbohydrates: 9,
+      protein: 1.1,
+      fat: 0.3,
+      calories: 29,
+      sugar: 2.5,
+    },
+  },
+  {
+    genus: "Malus",
+    name: "Apple",
+    id: 24,
+    family: "Rosaceae",
+    order: "Rosales",
+    nutritions: {
+      carbohydrates: 11.4,
+      protein: 0.3,
+      fat: 0.4,
+      calories: 52,
+      sugar: 10.3,
+    },
+  },
+];
 
-beforeEach(() => {
+describe("<HomePage />", () => {
+  it("should have multiple fruit names", async () => {
     const mockedFetchCall = fetchFruitInformation;
     mockedFetchCall.mockResolvedValue(sampleFruits);
 
     render(
       <MemoryRouter>
-          <HomePage selectFruit={jest.mock()} />
+        <HomePage selectFruit={jest.mock()} />
       </MemoryRouter>
     );
+
+    const lemon = await screen.findByText("Lemon");
+    const apple = await screen.findByText("Apple");
+
+    expect(lemon).toBeInTheDocument();
+    expect(apple).toBeInTheDocument();
   });
+});
