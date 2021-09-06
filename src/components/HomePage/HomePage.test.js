4fdeee0a1b9bcaeeb7089899addf2b21e33176e3
Mike Flynn <mdflynn34@outlook.com>
2021-09-06 09:04:52 -0600

    remove unneeded import

--- src/components/HomePage/HomePage.test.js
+++ src/components/HomePage/HomePage.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router";
 
