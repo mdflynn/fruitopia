8d765551c76af7f377f637558949dcdae9ca8d73
Mike Flynn <mdflynn34@outlook.com>
2021-09-05 19:46:37 -0600

    add test setup

---
+++ src/components/FruitPage/FruitPage.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router";
+
+import FruitPage from "./FruitPage";
+
+const sampleFruit = {
+  genus: "Citrus",
+  name: "Lemon",
+  id: 26,
+  family: "Rutaceae",
+  order: "Sapindales",
+  nutritions: {
+    carbohydrates: 9,
+    protein: 1.1,
+    fat: 0.3,
+    calories: 29,
+    sugar: 2.5,
+  },
+  image:
+    "https://passport-media.s3-us-west-1.amazonaws.com/images/eng-intern-interview/lemon.png",
+};
+
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <FruitPage fruitDetails={sampleFruit} />
+    </MemoryRouter>
+  );
+});
+
+
